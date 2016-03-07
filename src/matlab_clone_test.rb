759604d1850529f975604bafd2560f6c990bd1bd
[DEmjay <abdulmujeeb.jamiu@yahoo.com>
2016-03-07 23:52:56 +0100

    The RSpec test file for the aplication

--- src/matlab_clone_test.rb
+++ src/matlab_clone_test.rb
@@ -120,9 +120,9 @@ RSpec.describe "MatLab Clone" do
     #   expect(caller.save_output("save testfile.mat")).to eq nil
     # end
 
-    it 'should check for if file exist, if not, create a new file' do
-      expect(caller.save_output("save testfileff.mat")).to eq nil
-    end
+    # it 'should check for if file exist, if not, create a new file' do
+    #   expect(caller.save_output("save testfileff.mat")).to eq nil
+    # end
 
     it 'should check for incorrect save command format' do
       expect {caller.save_output('save new.ma')}.to raise_error 'Please save using this command: save filename.mat'.red
@@ -135,13 +135,13 @@ RSpec.describe "MatLab Clone" do
 
 
   context 'method load_data' do
-    # it 'should check for if file exist and load it' do
-    #   expect(caller.load_data("load testfile.mat")).to eq nil
-    # end
+    it 'should check for if file exist and load it' do
+      expect(caller.load_data("load testfile.mat")).to eq nil
+    end
 
-    # it 'should check for if file exist and flag error' do
-    #   expect(caller.load_data("load testfileff.mat")).to eq nil
-    # end
+    it 'should check for if file exist and flag error' do
+      expect(caller.load_data("load testfileff.mat")).to eq nil
+    end
 
     it 'should check for incorrect load command format' do
       expect {caller.load_data('load new.ma')}.to raise_error 'Please load_data using this command: load_data filename.mat'.red
